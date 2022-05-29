0cb10f7aa1fd24821b1f71016593bab6c3ac2e05
zerofaction <erin.wallaard@outlook.com>
2022-05-29 16:56:08 +0200

    First Commit

---
+++ Assets/_App/_Scripts/Tile.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile
+{
+    public int id;
+    public bool canTakeOver;
+    public int resources = 0;
+}
